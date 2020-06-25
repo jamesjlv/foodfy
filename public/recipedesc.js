47e8e3f0b52004ea87840365fb572ff21b9985fc
James Leal <james.leal2@gmail.com>
2020-06-24 21:06:37 -0300

    Adicionando função ao botão mostrar e esconder da página recipe_desc

--- public/recipedesc.js
+++ public/recipedesc.js
@@ -2,11 +2,14 @@ const items = document.querySelectorAll("span")
 
 for (let item of items) {
   item.addEventListener("click", () => {
-    console.log("ok");
+    const classe = item.parentElement.nextSibling.nextSibling.className;
+    const aux = document.getElementsByClassName(classe);
+    aux[0].classList.toggle('active');
+    const aux2 = item.innerText;
+    if (aux2 == "Esconder") {
+      item.innerText = "Mostrar";
+    } else {
+      item.innerText = "Esconder";
+    }
   })
-
-  // const itemClasse = item.className
-  // itemClasse.addEventListener("click", () => {
-  //   console.log("ok");
-  // })
 }
